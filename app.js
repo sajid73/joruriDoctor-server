81de2e34ef774f69d86a3e9b76d3bf551872381e
sajid73 <smsajid7300@gmail.com>
2022-08-07 12:51:12 +0600

    cors options updated2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const appointment = require("./routes/appointment.route");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://joruri-doctor.netlify.app'],
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 }
